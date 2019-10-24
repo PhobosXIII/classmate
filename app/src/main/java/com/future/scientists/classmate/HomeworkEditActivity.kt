3d1bcba30d1d580de3e3acc062428c3f1bd05901
OyamadoKun <SeventySeven_!43!>
2019-10-24 18:47:05 +0700

    step 1

--- app/src/main/java/com/future/scientists/classmate/HomeworkEditActivity.kt
+++ app/src/main/java/com/future/scientists/classmate/HomeworkEditActivity.kt
@@ -1,8 +1,11 @@
 package com.future.scientists.classmate
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.mikepenz.fastadapter.adapters.FastItemAdapter
 
 class HomeworkEditActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +14,23 @@ class HomeworkEditActivity : AppCompatActivity(){
         val title = intent?.extras?.getString(EXTRA_TITLE)
         val textView = findViewById<TextView>(R.id.tvTitle)
         textView.text = title
+
+        val actionBar = supportActionBar
+        actionBar!!.title = "Домашнее задание"
+        actionBar.setDisplayHomeAsUpEnabled(true)
+        actionBar.setDisplayHomeAsUpEnabled(true)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            startActivity(
+                    Intent(this, HomeworkEditActivity2::class.java)
+            )
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
 
