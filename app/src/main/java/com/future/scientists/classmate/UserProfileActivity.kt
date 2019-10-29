4b9204e41fef6d5564cd4bbb2b6168af3a01b3cc
youngmakne <ermolusya@gmail.com>
2019-10-29 18:55:22 +0700

    Magic commit

--- app/src/main/java/com/future/scientists/classmate/UserProfileActivity.kt
+++ app/src/main/java/com/future/scientists/classmate/UserProfileActivity.kt
@@ -1,12 +1,18 @@
 package com.future.scientists.classmate
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class UserProfileActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_profile)
+    }
+
+    fun startHomeworks(view: View) {
+        startActivity(Intent(this, HomeworksActivity::class.java))
     }
 }
