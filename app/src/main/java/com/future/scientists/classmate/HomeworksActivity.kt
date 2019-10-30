937f186dde27dbe2deeb5b3abac35eab9b50cd9f
Njves <mrpostik@gmail.com>
2019-10-30 18:52:10 +0700

    add homeworks screen

--- app/src/main/java/com/future/scientists/classmate/HomeworksActivity.kt
+++ app/src/main/java/com/future/scientists/classmate/HomeworksActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.mikepenz.fastadapter.adapters.FastItemAdapter
 
 class HomeworksActivity : AppCompatActivity() {
@@ -24,24 +25,23 @@ class HomeworksActivity : AppCompatActivity() {
             true
         }
 
+        val button = findViewById<FloatingActionButton>(R.id.edit)
+        button.setOnClickListener {
+            startActivity(Intent(this, HomeworkEditActivity::class.java))
+        }
+
         val recyclerView = findViewById<RecyclerView>(R.id.rvHomeworks)
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
         recyclerView.adapter = adapter
 
-        val homeworks = listOf(
-            HomeworkItem("History", "Create text about Caesar."),
-            HomeworkItem("English", "Anything"),
-            HomeworkItem("Maths", "2+2=4"),
-            HomeworkItem("Biology", "Read about T-Rex"),
-            HomeworkItem("History", "Create text about Caesar."),
-            HomeworkItem("English", "Anything"),
-            HomeworkItem("Maths", "2+2=4"),
-            HomeworkItem("Biology", "Read about T-Rex"),
-            HomeworkItem("History", "Create text about Caesar."),
-            HomeworkItem("English", "Anything"),
-            HomeworkItem("Maths", "2+2=4"),
-            HomeworkItem("Biology", "Read about T-Rex")
+        val homeworks = listOf<HomeworkItem>(
+            HomeworkItem(" ", "Русский язык", "Упражнение 14, стр. 7", "30 Октября"),
+            HomeworkItem(" ", "Литература", "Изучить биографию А.С.Пушкина", "30 Октября"),
+            HomeworkItem(" ", "Алгебра", "№ 3.11 - 3.19", "30 Октября"),
+            HomeworkItem(" ", "Геометрия", "Выучить теорему Пифагора", "30 Октября"),
+            HomeworkItem(" ", "Английский язык", "Выучить слова. Ex 9, p.5", "30 Октября"),
+            HomeworkItem(" ", "История", "Сделать таблицу о Наполеоне", "30 Октября")
         )
         adapter.setNewList(homeworks)
     }
